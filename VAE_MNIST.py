f396a22fb75a167644e25d34de6d8894a7f19007
peria1 <37117878+peria1@users.noreply.github.com>
2019-07-16 14:56:05 -0700

    Added display_bottleneck. added default values or nc, nr, and s in displayimages. Moved call of display_images to main loop.

--- VAE_MNIST.py
+++ VAE_MNIST.py
@@ -22,8 +22,8 @@ import time
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 100)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -75,18 +75,23 @@ class VAE(nn.Module):
         mu, logvar = self.encode(x.view(-1, 784))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
+    
+    def generate(self):  
+        pass
+        #  Need something like z = linspace(-sigma,sigma,...), then self.decode(z)
+        #   Want to raster scan the decode function via its inputs, rather than 
+        #    sampling randomly. 
+        #
 
 if 'model' not in locals():
+    print('new randomly initialized model...\n')
     model = VAE().to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=1e-5)
-
+beta = 0.5
 
 # Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    
-    beta = 0.01
-    
+def loss_function(recon_x, x, mu, logvar, beta):
     
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
 
@@ -121,15 +126,15 @@ def acquire_data_hook(self, input_tuple, output_tensor):    # Now THIS is a hook
 
 acq_hook_handle = model.fc4.register_forward_hook(acquire_data_hook)
 
-def display_images(img):
+def display_images(img, nr=8, nc=16, s=28):
     # img is a tensor containing a stack of images, shaped the 
     #  way pytorch does it, i.e. number of examples by number of 
     #  channels by number of rows by number of columns. The MNIST images are
     #  grayscale images, i.e. they have one channel only. 
     #
-    nr=8
-    nc=16
-    s=28 #side of a square (28by28)
+    #  The default nr, nc, s works for data acquired by forward  
+    #    hook from fc4 in class VAE. 
+    #
     img_np=img.cpu().detach().numpy()
     new_img=np.reshape(img_np, (nr*nc,s,s))
     disp=np.zeros((nr*s,nc*s))
@@ -141,8 +146,33 @@ def display_images(img):
                 c0=j*s
                 disp[r0:(r0+s),c0:(c0+s)]=new_img[read_data,:,:]
     plt.imshow(disp)
-    plt.pause(0.05) 
-  
+    plt.pause(0.05) # makes sure plt flushes its buffer. 
+
+def display_bottleneck(axes):
+    for batch_idx, (data, which_digit) in enumerate(train_loader):
+        break
+
+    fc21current,fc22current = model.encode(data.cuda().view(-1,784))
+
+    fc21disp = np.zeros((10,20))
+    fc22disp = np.zeros((10,20))
+
+    for i in range(10):
+        fc21disp[i,:] = \
+        np.sum(fc21current[which_digit==i,:].\
+               cpu().detach().numpy(),axis=0)
+        fc22disp[i,:] = \
+        np.sum(fc22current[which_digit==i,:].\
+               cpu().detach().numpy(),axis=0)
+
+    axes[0].imshow(fc21disp)
+    axes[1].imshow(fc22disp)
+    
+    plt.pause(0.05)
+    
+    return
+
+
 def train(epoch):
     model.train()
     train_loss = 0
@@ -154,7 +184,7 @@ def train(epoch):
         # For a given data object, mu and logvar are fixed, but
         #   recon_batch has stochasticity. 
         recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+        loss = loss_function(recon_batch, data, mu, logvar, beta)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
@@ -168,7 +198,8 @@ def train(epoch):
     print('====> Epoch: {} Average loss: {:.4f}'.format(
           epoch, train_loss / len(train_loader.dataset)))
 
-    display_images(ACQUIRED_DATA)
+#    display_images(ACQUIRED_DATA)
+    
     torch.save(model.state_dict(),'VAE' + date_for_filename())
 
     
@@ -179,7 +210,7 @@ def test(epoch):
         for i, (data, _) in enumerate(test_loader):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            test_loss += loss_function(recon_batch, data, mu, logvar, beta).item()
             if i == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
@@ -208,11 +239,20 @@ def date_for_filename():
 
 
 if __name__ == "__main__":
+    if "fc2fig" not in locals():
+        fc2fig, fc2axes = plt.subplots(2,1)
+
+    if "fc4fig" not in locals():
+        fc4fig = plt.figure()
+        
     for epoch in range(1, args.epochs + 1):
         train(epoch)
+        display_bottleneck(fc2axes)
+        plt.figure(fc4fig.number)
+        display_images(ACQUIRED_DATA)
         test(epoch)
-        with torch.no_grad():
-            sample = torch.randn(64, 20).to(device)
-            sample = model.decode(sample).cpu()
-            save_image(sample.view(64, 1, 28, 28),
-                       'VAEresults/sample_' + str(epoch) + '.png')
+#        with torch.no_grad():
+#            sample = torch.randn(64, 20).to(device)
+#            sample = model.decode(sample).cpu()
+#            save_image(sample.view(64, 1, 28, 28),
+#                       'VAEresults/sample_' + str(epoch) + '.png')
